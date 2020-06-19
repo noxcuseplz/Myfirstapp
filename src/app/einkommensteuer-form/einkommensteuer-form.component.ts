cac6553113916cfa477a7e5f0a9d44a69d5bd912
Matija Kovacevic <ithan@Matijas-MacBook-Air.local>
2020-06-19 20:12:06 +0200

    minimale anderung

--- src/app/einkommensteuer-form/einkommensteuer-form.component.ts
+++ src/app/einkommensteuer-form/einkommensteuer-form.component.ts
@@ -12,8 +12,10 @@ import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@ang
 export class EinkommensteuerFormComponent implements OnInit{
 
   gesellschaftsformoptionen: string[] = ['Einzelunternehmer', 'OG', 'KG', 'GmbH', 'AG'];
-  sitzoptionen: string[] = ['ja', 'nein'];
-  incomeOptions: string[] = ['Dividende', 'Option2'];
+  kgoptionen: string[] = ['Komplementär', 'Kommanditist'];
+  sitzoptionen: string[] = ['Österreich', 'EU', 'Drittland'];
+  incomeOptions: string[] = ['Dividende', 'Miete', 'Geschäftsführergehalt', 'Veräußerungsgewinn'];
+  expenseOptions: string [] = ['Kreditzinsen'];
   model: Einkommensteuer;
   ergebnis: number;
   submitted = false;
@@ -51,14 +53,28 @@ export class EinkommensteuerFormComponent implements OnInit{
   calculation() {
     if (this.model.gesellschaftsform === 'AG' && this.model.sitz === 'ja') {
       this.ergebnis = this.model.einkommen * 0.25;
-      } else { if (this.model.gesellschaftsform === 'GmbH' && this.model.sitz === 'ja') {
+      } else { if (this.model.gesellschaftsform === 'GmbH' && this.model.sitz === 'Österreich') {
         this.ergebnis = this.model.einkommen * 0.25;
-        } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'ja' && this.model.einkommen < 11000) {
+        } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen < 11000) {
           this.ergebnis = 0;
+          } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen < 18000) {
+            this.ergebnis = (this.model.einkommen - 11000) * 0.25 ;
+            } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen < 31000) {
+              this.ergebnis = (this.model.einkommen - 18000) * 0.35 + 1750;
+              } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen < 60000) {
+                this.ergebnis = (this.model.einkommen - 31000) * 0.42 + 6300;
+                } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen < 90000) {
+                  this.ergebnis = (this.model.einkommen - 60000) * 0.48 + 18480;
+                  } else { if (this.model.gesellschaftsform === 'OG' && this.model.sitz === 'Österreich' && this.model.einkommen >= 90000) {
+                    this.ergebnis = (this.model.einkommen - 90000) * 0.5 + 32880;
+                  }
+                }
+              }
+            }
+          } 
         }
       }
     }
   }
-
 }
 
